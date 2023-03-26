b65a2ff3b48fd72b51ac6ea398094d2ee03a68bf
ehsan110alavi <e.alavi1380@gmail.com>
2023-03-27 03:16:47 +0430

    first

--- src/component/body/Body.js
+++ src/component/body/Body.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Categorys from '../categorys/Categorys'
+import Discribtion from '../discribtion/Discribtion'
+import Info from '../infografy/Info'
 import "./Body.css"
 
 const Body = () => {
   return <>
         <Categorys/>
+        <Discribtion/>
+        <Info/>
   </>
 }
 
---
+++ src/component/infografy/Info.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Info.css"
+
+const Info = () => {
+  return <>
+    <div className="inf">
+        <div className="logos">
+            <img src="img/inf/logo.png"/>
+            <img src="img/inf/logo (1).png"/>
+            <img src="img/inf/logo-kasbokar.png"/>
+        </div>
+        <div className="inf-g">
+            <p>تمامی کالاها و خدمات این سایت، دارای مجوزهای لازم از مراجع مربوطه می‌باشند و فعالیت‌های این سایت تابع قوانین و مقررات جمهوری اسلامی ایران است.</p>
+        </div>
+    </div>
+  </>
+}
+
+export default Info
