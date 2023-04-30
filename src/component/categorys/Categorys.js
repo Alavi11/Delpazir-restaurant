6728de3209fa64f7384be3136bb7bea874733072
ehsan110alavi <e.alavi1380@gmail.com>
2023-04-30 21:10:19 +0330

    first

--- src/component/categorys/Categorys.js
+++ src/component/categorys/Categorys.js
@@ -35,19 +35,16 @@ const Categorys = () => {
                 {
                     category.map((item)=>{
                         return <>
-<<<<<<< HEAD
                             <Link to={`/${item.cat}`} className="food">
                                     <img src={item.img}/>
                                     <h3>{item.sub}</h3>
                             </Link>
-=======
+
                   
                                 <div>
                                     <img src={item.img}/>
                                     <h3>{item.sub}</h3>
                                 </div>
-                      
->>>>>>> 8926a4fbf9b3d0a6cf8611264384e68cad6c79e6
                         </>
                     })
                 }
